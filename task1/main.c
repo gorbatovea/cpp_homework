bc87213968d2a7c0c3a57d52a952be7df2ed6692
gorbatovea <gorbatov.eg@gmail.com>
2016-02-20 13:24:20 +0300

    Update main.c

--- task1/main.c
+++ task1/main.c
@@ -6,22 +6,25 @@ int main() {
     scanf("%d", &n);
     int x1,x2,y1,y2;
     n++;
-    scanf("%d %d %d %d", &x1,&y1,&x2,&y2);
-    int *ptrTable;
-    ptrTable = (int*) malloc(n*n*sizeof(int));
+    scanf("%d",&x1);
+    while(x1 != 0) {
+        scanf("%d %d %d", &y1, &x2, &y2);
+        int *ptrTable;
+        ptrTable = (int *) malloc(n * n * sizeof(int));
 
-    for (int i = y1;i<=y2; i++){
-        for (int j = x1; j<=x2; j++){
-            *(ptrTable + i*n+j) = i*j;
+        for (int i = y1; i <= y2; i++) {
+            for (int j = x1; j <= x2; j++) {
+                *(ptrTable + i * n + j) = i * j;
+            }
         }
-    }
 
-    for (int i = y1;i<=y2; i++){
-        for (int j = x1; j<=x2; j++){
-            printf("%d", *(ptrTable + i*n+j));
+        for (int i = y1; i <= y2; i++) {
+            for (int j = x1; j <= x2; j++) {
+                printf("%d", *(ptrTable + i * n + j));
+            }
+            printf("\n");
         }
-        printf("\n");
+        free(ptrTable);
+        return 0;
     }
-    free(ptrTable);
-    return 0;
 }
