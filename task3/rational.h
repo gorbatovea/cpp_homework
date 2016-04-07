5fd994a39fd4dcfb83cb657d4f8884aad03e30b8
gorbatovea <gorbatov.eg@gmail.com>
2016-04-07 12:39:20 +0300

    Added files via upload

--- task3/rational.h
+++ task3/rational.h
@@ -4,12 +4,17 @@
 
 #ifndef RATIONAL_RATIONAL_H
 #define RATIONAL_RATIONAL_H
+
+#include <cstdio>
+#include <cstdlib>
+
 class rational{
 private:
+    bool positive;
     int num;
     int denom;
 
-    int gcd(int m, int n) const
+    int greatestCommonDivisor(int m, int n)
     {
         int r;
         if((m == 0) || (n == 0))
@@ -30,84 +35,100 @@ private:
         return n;
     }
 
-    int greatestCommonDivisor(int n, int d) const{
-        if (n < 0) {
-            n = -n;
-            return gcd(n,d); }
-        if (d < 0) {
-            d = -d;
-            return gcd(n,d);
-        }
-        return gcd(n,d);
-    };
-
 public:
     rational(){
+        this -> positive = true;
         this -> num = 0;
         this -> denom = 0;
     }
     rational(int n){
-        this -> num = n;
-        this -> denom = 1;
+        if (n < 0){
+            this -> positive =false;
+            this -> num = -n;
+            this -> denom = 1;
+        }
+        else{
+            this -> positive = true;
+            this -> num = n;
+            this -> denom = 1;
+        }
+
     }
     rational(int n, int d){
-        this -> num = n;
-        this -> denom =d;
-        if ((denom < 0) && (num > 0)) {
-            num = -num;
-            denom = -denom;
-        }
-        int gcd = greatestCommonDivisor(num, denom);
-        num = num / gcd;
-        denom = denom / gcd;
-    }
+        if ((n < 0) || (d < 0)) {
+            this -> positive = false;
+            if (n < 0) {
+                this->num = -n;
+                this->denom = d;
+                int gcd = greatestCommonDivisor(num, denom);
+                num = num / gcd;
+                denom = denom / gcd;
+            }
+            else {
+                this->num = n;
+                this->denom = -d;
+                int gcd = greatestCommonDivisor(num, denom);
+                num = num / gcd;
+                denom = denom / gcd;
+            }
 
+        } else {
+            this -> positive = true;
+            this->num = n;
+            this->denom = d;
+            int gcd = greatestCommonDivisor(num, denom);
+            num = num / gcd;
+            denom = denom / gcd;
+        }
+
+    }
     int getNum() const{
+        if (this -> positive == false) return -num;
         return this -> num;
     }
     int getDenom() const {
         return this -> denom;
     }
 
-    rational operator+(const rational& second) const{
+    rational operator+(const rational& second) {
         rational result;
-        result.denom = this -> denom * second.denom;
-        int multi_first = result.denom / this -> denom;
-        int multi_sec = result.denom / second.denom;
-        result.num = (this -> num * multi_first) + (second.num * multi_sec);
-        int gcd = greatestCommonDivisor(result.num , result.denom);
-        int temp = result.num; result.num = temp / gcd;
-        temp = result.denom; result.denom = temp / gcd;
+        if (this -> positive == true) {
+            result.denom = this->denom * second.denom;
+            int multi_first = result.denom / this->denom;
+            int multi_sec = result.denom / second.denom;
+            result.num = (this->num * multi_first) + (second.num * multi_sec);
+            int gcd = greatestCommonDivisor(result.num, result.denom);
+            int temp = result.num;
+            result.num = temp / gcd;
+            temp = result.denom;
+            result.denom = temp / gcd;
+        }
+        if (this -> positive == false){
+            result.positive = false;
+            result.denom = this->denom * second.denom;
+            int multi_first = result.denom / this->denom;
+            int multi_sec = result.denom / second.denom;
+
+        }
         return result;
     }
 
-    rational operator- (const rational& second) const{
+    rational operator- (const rational& second){
         rational result;
         result.denom = this -> denom * second.denom;
         int multi_first = result.denom / this -> denom;
         int multi_sec = result.denom / second.denom;
         result.num = (this -> num * multi_first) - (second.num * multi_sec);
-        int gcd = greatestCommonDivisor(result.num , result.denom);
-        int temp = result.num; result.num = temp / gcd;
-        temp = result.denom; result.denom = temp / gcd;
-        return result;
-}
-
-    rational operator* (const rational& second) const{
-        rational result;
-        result.denom = this -> denom * second.denom;
-        result.num = this -> num * second.num;
-        int gcd = greatestCommonDivisor(result.num,result.denom);
-        result.num = result.num / gcd;
-        result.denom = result.denom /gcd;
         return  result;
     }
 
-    rational operator/ (const rational& second) const{
+    rational operator* (const rational& frac){
         rational result;
-        result.num = second.denom;
-        result.denom = second.num;
-        result = result * *this;
+        return  result;
+    }
+
+    rational operator/ (const rational& frac){
+        rational result;
         return  result;
     }
 };
